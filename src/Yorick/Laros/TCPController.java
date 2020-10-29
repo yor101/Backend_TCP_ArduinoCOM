dd3cf3f05e6c0d73be0d4d2ae79847c1aa722c14
yorick <yorick_laros@outlook.com>
2020-10-29 18:47:13 +0100

    added a list for stor the datas

--- src/Yorick/Laros/TCPController.java
+++ src/Yorick/Laros/TCPController.java
@@ -4,34 +4,40 @@ package Yorick.Laros;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-import java.util.Timer;
+
 
 public class TCPController extends Thread{
 
+    private final List<String> myStringList = new ArrayList<>();
+
 
     public void TCPHandler(int portNumber) {
+
+        //char STX = 2;
+        //char ETX = 3;
+
         try {
             ServerSocket serverSocket = new ServerSocket(portNumber);
             Socket socket = serverSocket.accept();
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            //PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             if (socket.isBound()) {
                 System.out.println("connected");
             }
 
             int value;
-                        String message;
+            String message;
 
             while ((value = in.read()) != -1) {
                 //c = (char)value;
                 message = Character.toString((char) value);
+                myStringList.add(message);
                 System.out.println(message);
-
             }
 
         } catch (
